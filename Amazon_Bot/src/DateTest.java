a9e3ee5f196074a0bea1d29b7d4e93f6bbf80f1a
Junrybuenavista <42229618+Junrybuenavista@users.noreply.github.com>
2021-10-30 12:22:20 +0800

    f

--- Amazon_Bot/src/DateTest.java
+++ Amazon_Bot/src/DateTest.java
@@ -10,21 +10,23 @@ public class DateTest {
 	public DateTest() {
 		 try {
 				SimpleDateFormat dateformatDay = new SimpleDateFormat("EEEEE");
+				SimpleDateFormat dateformat2 = new SimpleDateFormat("MM/dd/yyyy");
 				
 				SimpleDateFormat dateformat3 = new SimpleDateFormat("yyyy-MM-dd");
 				
 				Date date1 = dateformat3.parse("2021-10-15");
 
 				Date date = new Date();
-		        String todate = dateformat3.format(date);
+		        String todate = dateformat2.format(date);
 
 		        Calendar cal = Calendar.getInstance();
-		        cal.add(Calendar.DATE, -7);
+		        cal.add(Calendar.DATE, -2);
+		        
 		        Date todate1 = cal.getTime();    
 		        String fromdate = dateformat3.format(todate1);
 				
-				System.out.println("From:"+fromdate);
-				System.out.println("to:"+todate);
+				System.out.println("current:"+todate);
+				System.out.println("-2:"+fromdate);
 				System.out.println("Today is:"+dateformatDay.format(new Date()));
 				
 				
